54638b943bf0827a47ba22c28afd3ff399068567
bona.vensius <bonavensius72@gmail.com>
2020-09-29 21:18:26 +0700

    first commit

---
+++ database/migrations/2020_09_29_125858_create_attendances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("absent_from");
+            $table->dateTime("absent_to");
+            $table->string("cutoff");
+            $table->text("attachment");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
